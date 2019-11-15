b729ec5d14535af36a56250343986e9da9375b9d
varlackc <varlackc@mail.gvsu.edu>
2019-11-14 23:30:35 -0600

    Update Store Model
    Update Store Model

--- CarRepairApp/Models/StoreModel.cs
+++ CarRepairApp/Models/StoreModel.cs
@@ -7,5 +7,10 @@ namespace CarRepairApp.Models
 {
     public class StoreModel
     {
+        int StoreId { get; set; }
+        string StoreName { get; set; }
+        string StoreAddress { get; set; }
+        string PhoneNumber { get; set; }
+        string HoursOfOperation { get; set; }
     }
 }
