9de1b7bc2209427680523946a66460b625b3a61e
varlackc <varlackc@mail.gvsu.edu>
2019-11-14 23:37:49 -0600

    Update Order Line Model
    Update Order Line Model

--- CarRepairApp/Models/OrderLineModel.cs
+++ CarRepairApp/Models/OrderLineModel.cs
@@ -7,5 +7,15 @@ namespace CarRepairApp.Models
 {
     public class OrderLineModel
     {
+        int OrderLineId { get; set; }
+        int OrderId { get; set; }
+        int PartId { get; set; }
+        int ServiceId { get; set; }
+        int LineNo { get; set; }
+        string LineDescription { get; set; }
+        int ServiceQty { get; set; }
+        int PartQty { get; set; }
+        string Status { get; set; }
+        string OrderNotes { get; set; }
     }
 }
