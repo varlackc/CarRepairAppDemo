f5fc071525386dc658174277b3182039266be59d
varlackc <varlackc@mail.gvsu.edu>
2019-11-14 23:27:33 -0600

    Update Employee Model
    Update Employee Model

--- CarRepairApp/Models/EmployeeModel.cs
+++ CarRepairApp/Models/EmployeeModel.cs
@@ -7,5 +7,12 @@ namespace CarRepairApp.Models
 {
     public class EmployeeModel
     {
+        int EmployeeId { get; set; }
+        string UserName { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Location { get; set; }
+        string PhoneNumber { get; set; }
+        string Status { get; set; }
     }
 }
