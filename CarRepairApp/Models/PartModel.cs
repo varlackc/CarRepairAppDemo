2ed92481b16eb6d672aeda986e69c17a435d0c92
varlackc <varlackc@mail.gvsu.edu>
2019-11-14 23:42:20 -0600

    Update Part Model
    Update Part Model

--- CarRepairApp/Models/PartModel.cs
+++ CarRepairApp/Models/PartModel.cs
@@ -7,5 +7,12 @@ namespace CarRepairApp.Models
 {
     public class PartModel
     {
+        int PartId { get; set; }
+        string PartName { get; set; }
+        string PartDescription { get; set; }
+        string PartNumber { get; set; }
+        string PartSupplier { get; set; }
+        decimal PartCost { get; set; }
+        decimal PartPrice { get; set; } 
     }
 }
