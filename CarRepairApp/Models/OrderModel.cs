c63b2f7c0086056d17e7bcdc83326958f638bc9f
varlackc <varlackc@mail.gvsu.edu>
2019-11-14 23:24:41 -0600

    update order model
    update order model

--- CarRepairApp/Models/OrderModel.cs
+++ CarRepairApp/Models/OrderModel.cs
@@ -7,5 +7,14 @@ namespace CarRepairApp.Models
 {
     public class OrderModel
     {
+        int ClientId { get; set; }
+        int StoreId { get; set; }
+        int EmployeeId { get; set; }
+        DateTime OrderTime { get; set; }
+        string OrderType { get; set; }
+        string OrderSpecifications { get; set; }
+        string Description { get; set; }
+        string Location { get; set; }
+        string Status { get; set; }
     }
 }
