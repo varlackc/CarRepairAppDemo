7f2b7889f643cca19b2236cee2dbf1857c2e9a40
varlackc <varlackc@mail.gvsu.edu>
2019-11-14 23:33:54 -0600

    Update Client Model
    Update the client model

--- CarRepairApp/Models/ClientModel.cs
+++ CarRepairApp/Models/ClientModel.cs
@@ -7,5 +7,12 @@ namespace CarRepairApp.Models
 {
     public class ClientModel
     {
+        int ClientId { get; set; }
+        string UserName { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Location { get; set; }
+        string PhoneNumber { get; set; }
+        string Status { get; set; }
     }
 }
