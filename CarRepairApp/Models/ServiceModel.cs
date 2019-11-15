555e8979b83ce98dd5a5c38ec4228b6fe5aa8282
varlackc <varlackc@mail.gvsu.edu>
2019-11-14 23:45:38 -0600

    Update Service Model
    Update Service Model

--- CarRepairApp/Models/ServiceModel.cs
+++ CarRepairApp/Models/ServiceModel.cs
@@ -7,5 +7,10 @@ namespace CarRepairApp.Models
 {
     public class ServiceModel
     {
+        int ServiceId { get; set; }
+        string ServiceName { get; set; }
+        string ServiceDescription { get; set; }
+        DateTime ServiceDate { get; set; }
+        string Status { get; set; }
     }
 }
