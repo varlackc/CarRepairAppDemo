533681fa8507669bb462e49b3e8ec8db37deacaf
varlackc <varlackc@mail.gvsu.edu>
2019-11-22 23:50:04 -0600

    Update Order Model data

--- DataLibrary/BusinessLogic/OrderProcessor.cs
+++ DataLibrary/BusinessLogic/OrderProcessor.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataLibrary.DataAccess;
+using DataLibrary.Models;
 
 namespace DataLibrary.BusinessLogic
 {
     class OrderProcessor
     {
         //Create new Order
-        public static int CreateOrder()
+        public static int CreateOrder(int clientId, int storeId, int employeeId, DateTime orderTime, 
+                                    string orderType, string orderSpecifications, string description, 
+                                    string location, string status)
         {
-
+            OrderModel data = new OrderModel
+            {
+                ClientId = clientId, 
+                StoreId = storeId,
+                EmployeeId = employeeId, 
+                OrderTime = orderTime, 
+                OrderType = orderType, 
+                OrderSpecifications = orderSpecifications, 
+                Description = description, 
+                Location = location, 
+                Status = status
+            };
         }
     }
 }
