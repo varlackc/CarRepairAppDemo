3125457c6fa9e5202ace6492b4100eb19ea412fb
varlackc <varlackc@mail.gvsu.edu>
2019-12-05 23:59:04 -0600

    Update ServiceProcessor.cs
    Modify section

--- DataLibrary/BusinessLogic/ServiceProcessor.cs
+++ DataLibrary/BusinessLogic/ServiceProcessor.cs
@@ -12,6 +12,67 @@ namespace DataLibrary.BusinessLogic
     public static class ServiceProcessor
     {
         //create new Service
-        
+
+        public static int CreateService(int storeId, string storeName, string storeAddress, string phoneNumber,
+                                      string hoursOfOperation)
+        {
+            StoreModel data = new StoreModel
+            {
+                StoreId = storeId,
+                StoreName = storeName,
+                StoreAddress = storeAddress,
+                PhoneNumber = phoneNumber,
+                HoursOfOperation = hoursOfOperation
+            };
+
+            string sql = @"Insert into dbo.[Store](StoreId, StoreName, StoreAddress, PhoneNumber, HoursOfOperation) *
+                           Values (@StoreId, @StoreName, @StoreAddress, @PhoneNumber, @HoursOfOperation);";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        //Read Part Data
+        public static List<PartModel> LoadService()
+        {
+
+            //create SQL Query
+            string sql = @"SELECT *
+                           FROM dbo.[Part];";
+
+            return SqlDataAccess.LoadData<PartModel>(sql);
+        }
+
+        //Update Store Data
+        public static void UpdateService(int storeId, string storeName, string storeAddress, string phoneNumber,
+                                     string hoursOfOperation)
+        {
+            PartModel data = new PartModel
+            {
+                StoreId = sotreId,
+                StoreName = storeName,
+                StoreAddress = storeAddress,
+                PhoneNumber = phoneNumber,
+                HoursOfOperation = hoursOfOperation
+            };
+
+            //create SQL Query
+            string sql = @"Update dbo.[Part]
+                           SET StoreId = @StoreId, StoreName = @StoreName, StoreAddress = @StoreAddress, 
+                               PhoneNumber = @PhoneNumber, HoursOfOperation = @HoursOfOperation
+                           WHERE StoreId = @StoreId;";
+
+            SqlDataAccess.UpdateData(sql, data);
+        }
+
+
+        //Delete Order
+        public static void DeleteService(int id)
+        {
+            //create the sql command
+            string sql = @"DELETE FROM dbo.[Store] WHERE StoreId = @id";
+
+            //Call the sql data access to delete the User entry
+            SqlDataAccess.DeleteData(sql, id);
+        }
     }
 }
