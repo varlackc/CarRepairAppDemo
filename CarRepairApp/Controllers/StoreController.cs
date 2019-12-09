42a66bbba1c1c024951f41cb582a9ae5349cfcbb
varlackc <varlackc@mail.gvsu.edu>
2019-12-08 23:24:09 -0600

    Update StoreController.cs

--- CarRepairApp/Controllers/StoreController.cs
+++ CarRepairApp/Controllers/StoreController.cs
@@ -3,9 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using CarRepairApp.Models;
+using static DataLibrary.BusinessLogic.StoreProcessor;
+
+
 namespace CarRepairApp.Controllers
 {
-    public class StoreController
+    public class StoreController : Controllers
     {
+        public ActionResult StoreList()
+        {
+            var data = LoadUser(); //load the data
+            List<UserModel> users = new List<UserModel>(); //create a list of projects
+            foreach (var row in data) // loop to organize the data in the projects list
+            {
+                users.Add(new UserModel
+                {
+                    Id = row.Id,
+                    UserName = row.UserName,
+                    FirstName = row.FirstName,
+                    LastName = row.LastName
+                });
+            }
+            return View(users);
+        }
     }
 }
