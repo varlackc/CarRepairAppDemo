20f4ea141cd2ad97fd4ad82c0ce488348c8f97f0
varlackc <varlackc@mail.gvsu.edu>
2019-12-08 23:25:15 -0600

    Update StoreController.cs

--- CarRepairApp/Controllers/StoreController.cs
+++ CarRepairApp/Controllers/StoreController.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.Web.Mvc;
 using CarRepairApp.Models;
 using static DataLibrary.BusinessLogic.StoreProcessor;
 
 
 namespace CarRepairApp.Controllers
 {
-    public class StoreController : Controllers
+    public class StoreController : Controller
     {
         public ActionResult StoreList()
         {
