aeec4339eeb991ed5ec0ca9f073ec00bd60b947e
varlackc <varlackc@mail.gvsu.edu>
2019-12-13 23:15:38 -0600

    Add data library to order controller

--- CarRepairApp/Controllers/OrderController.cs
+++ CarRepairApp/Controllers/OrderController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using CarRepairApp.Models;
+using static DataLibrary.BusinessLogic.OrderProcessor;
+
 namespace CarRepairApp.Controllers
 {
     public class OrderController : Controller
