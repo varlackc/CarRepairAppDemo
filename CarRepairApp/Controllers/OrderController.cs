7beccc3eff840e10d1605469403b7fa340c9b732
varlackc <varlackc@mail.gvsu.edu>
2019-12-13 23:31:08 -0600

    Update OrderController.cs

--- CarRepairApp/Controllers/OrderController.cs
+++ CarRepairApp/Controllers/OrderController.cs
@@ -11,5 +11,21 @@ namespace CarRepairApp.Controllers
 {
     public class OrderController : Controller
     {
+        public ActionResult OrderList(int id)
+        {
+            var data = LoadOrder(id); //load the data
+            List<OrderModel> users = new List<OrderModel>(); //create a list of projects
+            foreach (var row in data) // loop to organize the data in the projects list
+            {
+                users.Add(new OrderModel
+                {
+                    OrderType = row.OrderType,
+                    OrderSpecifications = row.OrderSpecifications,
+                    OrderTime = row.OrderTime
+
+                });
+            }
+            return View(users);
+        }
     }
 }
