5362594a645909d2321f99699c2a3d5640f82ff4
varlackc <varlackc@mail.gvsu.edu>
2019-12-13 23:37:39 -0600

    Update EmployeeController.cs

--- CarRepairApp/Controllers/EmployeeController.cs
+++ CarRepairApp/Controllers/EmployeeController.cs
@@ -5,11 +5,31 @@ using System.Web;
 using System.Web.Mvc;
 
 using DataLibrary;
+using CarRepairApp.Models;
 using static DataLibrary.BusinessLogic.EmployeeProcessor;
 
 namespace CarRepairApp.Controllers
 {
     public class EmployeeController : Controller
     {
+
+        public ActionResult EmployeeList()
+        {
+            var data = LoadEmployee(); //load the data
+            List<EmployeeModel> users = new List<EmployeeModel>(); //create a list of projects
+            foreach (var row in data) // loop to organize the data in the projects list
+            {
+                users.Add(new EmployeeModel
+                {
+                    EmployeeId = row.EmployeeId, 
+                    FirstName = row.FirstName,
+                    LastName = row.LastName,
+                    Location = row.Location,
+                    PhoneNumber = row.Location,
+                    Status = row.Status
+                });
+            }
+            return View(users);
+        }
     }
 }
