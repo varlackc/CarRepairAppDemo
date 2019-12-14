c8105af8396bde6eeefeb3957b61156553d598f8
varlackc <varlackc@mail.gvsu.edu>
2019-12-13 23:25:14 -0600

    Update Part controller

--- CarRepairApp/Controllers/PartController.cs
+++ CarRepairApp/Controllers/PartController.cs
@@ -12,5 +12,25 @@ namespace CarRepairApp.Controllers
 {
     public class PartController : Controller
     {
+        public ActionResult PartList()
+        {
+            var data = LoadPart(); //load the data
+            List<PartModel> users = new List<PartModel>(); //create a list of projects
+            foreach (var row in data) // loop to organize the data in the projects list
+            {
+                users.Add(new PartModel
+                {
+                    PartId = row.PartId,
+                    PartName = row.PartName,
+                    PartNumber = row.PartNumber,
+                    PartDescription = row.PartDescription,
+                    PartCost = row.PartCost,
+                    PartPrice = row.PartPrice,
+                    PartSupplier = row.PartSupplier
+                    
+                });
+            }
+            return View(users);
+        }
     }
 }
