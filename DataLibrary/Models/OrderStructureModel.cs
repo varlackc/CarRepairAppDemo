aca279c870b59ef2022cf7ef77654b45f8ae94fc
varlackc <varlackc@mail.gvsu.edu>
2019-12-19 20:22:56 -0600

    Create OrderStructureModel.cs

---
+++ DataLibrary/Models/OrderStructureModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using DataLibrary.Models;
+
+namespace DataLibrary.BusinessLogic
+{
+    public class OrderStructure
+    {
+        public OrderModel OrderHeading { get; set; }
+        public List<OrderLineModel> OrderBody { get; set; }
+    }
+}
