756fc8eafddb9e15e81b3b5f64c0454ee77e6c29
varlackc <varlackc@mail.gvsu.edu>
2020-01-02 22:55:29 -0600

    Update ClientController.cs

--- CarRepairApp/Controllers/ClientController.cs
+++ CarRepairApp/Controllers/ClientController.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using DataLibrary;
-
 using CarRepairApp.Models;
 using static DataLibrary.BusinessLogic.ClientProcessor;
 
